d1053fba3db9145e5b7f6cee32c003212655a20e
Jurandir C. Gonçalves <juragoncalves@gmail.com>
2019-10-25 08:20:49 -0300

    correcao swagger

--- src/main/java/com/myh3alth/paciente/ws/service/rest/v1/RestApplication.java
+++ src/main/java/com/myh3alth/paciente/ws/service/rest/v1/RestApplication.java
@@ -37,7 +37,7 @@ import br.com.jgon.canary.ws.rest.CanaryRestResources;
 				)
 		),
 		servers = @Server(
-				url = "http://paciente.desenv.myh3alth.com"
+				url = "http://paciente-desenv.myh3alth.com"
 		)
 )
 @ApplicationPath("/api/v1/")
