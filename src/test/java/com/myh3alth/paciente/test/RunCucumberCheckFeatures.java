d1191168f4434883b783acc5600152cf1f5cdf6b
Jurandir C. Gonçalves <juragoncalves@gmail.com>
2019-10-28 18:14:41 -0300

    infraestrutura inicial

--- src/test/java/com/myh3alth/paciente/test/RunCucumberCheckFeatures.java
+++ src/test/java/com/myh3alth/paciente/test/RunCucumberCheckFeatures.java
@@ -2,10 +2,10 @@ package com.myh3alth.paciente.test;
 
 import org.junit.runner.RunWith;
 
-import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import io.quarkus.test.junit.QuarkusTestCucumber;
 
-@RunWith(Cucumber.class)
+@RunWith(QuarkusTestCucumber.class)
 @CucumberOptions(
 		plugin = {"pretty", "summary"}, 
 		features = "target/features",
