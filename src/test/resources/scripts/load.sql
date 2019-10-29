d62e9ca0bbe3b44eb2609c3d1a8b8cebe87dd03c
Jurandir C. Gonçalves <juragoncalves@gmail.com>
2019-10-29 11:04:17 -0300

    conf ambiente de banco de dados de testes

---
+++ src/test/resources/scripts/load.sql
@@ -0,0 +1,5 @@
+insert into paciente.paciente (id, nome, situacao) values (nextval('paciente.seq_paciente_id'), 'Paciente 1', 'A');
+insert into paciente.paciente (id, nome, situacao) values (nextval('paciente.seq_paciente_id'), 'Paciente 2', 'I');
+insert into paciente.paciente (id, nome, situacao) values (nextval('paciente.seq_paciente_id'), 'Paciente 3', 'A');
+insert into paciente.paciente (id, nome, situacao) values (nextval('paciente.seq_paciente_id'), 'Paciente 4', 'I');
+insert into paciente.paciente (id, nome, situacao) values (nextval('paciente.seq_paciente_id'), 'Paciente 5', 'A');
