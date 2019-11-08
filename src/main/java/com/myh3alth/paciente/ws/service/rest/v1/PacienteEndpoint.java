aac5c70e6e48be45432d00d14b14a63b6be33b10
Jurandir C. Gonçalves <juragoncalves@gmail.com>
2019-11-08 19:23:42 -0300

    criacao das entidades

--- src/main/java/com/myh3alth/paciente/ws/service/rest/v1/PacienteEndpoint.java
+++ src/main/java/com/myh3alth/paciente/ws/service/rest/v1/PacienteEndpoint.java
@@ -1,5 +1,7 @@
 package com.myh3alth.paciente.ws.service.rest.v1;
 
+import java.util.Date;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.ws.rs.DefaultValue;
@@ -26,8 +28,11 @@ import org.jboss.resteasy.annotations.GZIP;
 import com.myh3alth.paciente.business.PacienteBusiness;
 import com.myh3alth.paciente.entity.enumeration.Situacao;
 import com.myh3alth.paciente.util.Utils;
+import com.myh3alth.paciente.ws.service.rest.v1.request.V1RequestPaciente;
+import com.myh3alth.paciente.ws.service.rest.v1.response.V1ResponsePaciente;
 
 import br.com.jgon.canary.exception.ApplicationException;
+import br.com.jgon.canary.persistence.exception.SaveEntityException;
 import br.com.jgon.canary.util.Page;
 import br.com.jgon.canary.ws.rest.link.LinkPaginate;
 import br.com.jgon.canary.ws.rest.link.LinkResource;
@@ -49,12 +54,19 @@ import br.com.jgon.canary.ws.rest.util.DominiosRest;
 //Fault Tolerance
 @Retry(maxRetries = Utils.DEFAULT_RETRY_MAX_RETRIES)
 @Timeout(Utils.MEDIUM_TIMETOUT)
-@CircuitBreaker(delay = Utils.DEFAULT_CIRCUIT_BRAKER_DELAY, requestVolumeThreshold = Utils.DEFAULT_CIRCUIT_BRAKER_REQUEST_VOLUME_THRESHOLD,
-    failureRatio = Utils.DEFAULT_CIRCUIT_BRAKER_FAILURE_RATIO, successThreshold = Utils.DEFAULT_CIRCUIT_BRAKER_SUCCESS_THRESHOLD)
+@CircuitBreaker(
+        delay = Utils.DEFAULT_CIRCUIT_BRAKER_DELAY,
+        requestVolumeThreshold = Utils.DEFAULT_CIRCUIT_BRAKER_REQUEST_VOLUME_THRESHOLD,
+        failureRatio = Utils.DEFAULT_CIRCUIT_BRAKER_FAILURE_RATIO,
+        successThreshold = Utils.DEFAULT_CIRCUIT_BRAKER_SUCCESS_THRESHOLD)
 //Metrics
-@Counted(name = "endpointPaciente", displayName = "Endpoint Pacientes",
-    description = "Metrica que apresenta quantas vezes o endpoint de pacientes foi chamado")
-@Timed(name = "timeEndpointPaciente", description = "Metrica que apresenta o tempo de duração do evento do endpoint de paciente")
+@Counted(
+        name = "endpointPaciente",
+        displayName = "Endpoint Pacientes",
+        description = "Metrica que apresenta quantas vezes o endpoint de pacientes foi chamado")
+@Timed(
+        name = "timeEndpointPaciente",
+        description = "Metrica que apresenta o tempo de duração do evento do endpoint de paciente")
 //Open API
 @Tag(name = "Pacientes")
 public class PacienteEndpoint {
@@ -63,29 +75,44 @@ public class PacienteEndpoint {
     PacienteBusiness pacienteBusiness;
 
     /**
+     * 
+     * @author Jurandir C. Gonçalves <jurandir> - Zion Mountain
+     * @since 08/11/2019
+     *
+     * @param paciente - {@link V1RequestPaciente}
+     * @return {@link V1ResponsePaciente}
+     * @throws SaveEntityException {@link SaveEntityException}
+     */
+    public V1ResponsePaciente salvar(final V1RequestPaciente paciente) throws SaveEntityException {
+        return new V1ResponsePaciente().converter(pacienteBusiness.salvar(paciente.converter()));
+    }
+
+    /**
      * @author Jurandir C. Gonçalves <jurandir> - Zion Mountain
      * @since 22/10/2019
      *
-     * @param id {@link Long}
+     * @param id     {@link Long}
      * @param fields {@link List}
-     * @return {@link ResponsePaciente}
+     * @return {@link V1ResponsePaciente}
      * @throws ApplicationException {@link ApplicationException}
      */
     @GET
     @Path("/{id}")
     @Timed
-    @LinkResources(value = { @LinkResource(rel = "self", title = "Self", pathParameters = { "${id}" }),
-            @LinkResource(rel = "update", title = "Update", pathParameters = { "${id}" }),
-            @LinkResource(rel = "delete", title = "Delete", pathParameters = { "${id}" }),
-            @LinkResource(rel = "create", title = "Create", pathParameters = { "" }) })
+    @LinkResources(
+            value = { @LinkResource(rel = "self", title = "Self", pathParameters = { "${id}" }),
+                    @LinkResource(rel = "update", title = "Update", pathParameters = { "${id}" }),
+                    @LinkResource(rel = "delete", title = "Delete", pathParameters = { "${id}" }),
+                    @LinkResource(rel = "create", title = "Create", pathParameters = { "" }) })
     @Operation(summary = "obtém as informações do paciente")
-    @APIResponse(content = @Content(schema = @Schema(implementation = ResponsePaciente.class)))
-    public ResponsePaciente obterPaciente(@Parameter() @PathParam("id") final Long id,
-            @Parameter(schema = @Schema(implementation = String.class))
-                @QueryParam("fields") @DefaultValue("id,nome") @WSParamFormat(ResponsePaciente.class) final WSFieldParam fields)
+    @APIResponse(content = @Content(schema = @Schema(implementation = V1ResponsePaciente.class)))
+    public V1ResponsePaciente obterPaciente(@Parameter() @PathParam("id") final Long id, @Parameter(
+            schema = @Schema(
+                    implementation = String.class)) @QueryParam("fields") @DefaultValue("id,nome") @WSParamFormat(V1ResponsePaciente.class)
+            final WSFieldParam fields)
             throws ApplicationException {
 
-        return new ResponsePaciente().converter(pacienteBusiness.obterPaciente(id, fields.getListField()));
+        return new V1ResponsePaciente().converter(pacienteBusiness.obter(id, fields.getListField()));
     }
 
     /**
@@ -95,6 +122,8 @@ public class PacienteEndpoint {
      * @param id       {@link Long}
      * @param nome     {@link String}
      * @param situacao {@link Situacao}
+     * @param cpf      {@link String}
+     * @param dataNascimento {@link Date}
      * @param page     {@link Integer}
      * @param limit    {@link Integer}
      * @param fields   {@link List}
@@ -108,24 +137,38 @@ public class PacienteEndpoint {
     @Timed
     @LinkPaginate(pageParamName = "page", limitParamName = "limit", embeddedCollectionName = "pacientes")
     @Operation(summary = "paginação de pacientes conforme filtros aplicados")
-    @APIResponse(content = @Content(schema = @Schema(implementation = ResponsePaciente.class)))
-    public Page<ResponsePaciente> paginarPaciente(
+    @APIResponse(content = @Content(schema = @Schema(implementation = V1ResponsePaciente.class)))
+    public Page<V1ResponsePaciente> paginarPaciente(
             // @HeaderParam("Authorization") String token,
-            @Parameter() @QueryParam("id") final Long id,
-            @Parameter() @QueryParam("nome") final String nome,
-            @Parameter(schema = @Schema(implementation = String.class, enumeration = { "ATIVO", "INATIVO" }))
-                @QueryParam("situacao") final Situacao situacao,
-            @Parameter(schema = @Schema(implementation = String.class, defaultValue = "id,nome"))
-                @QueryParam("fields") @DefaultValue("id,nome") @WSParamFormat(ResponsePaciente.class) final WSFieldParam fields,
-            @Parameter(schema = @Schema(implementation = String.class, defaultValue = "nome:asc"))
-                @QueryParam("sort") @DefaultValue("nome:asc") @WSParamFormat(ResponsePaciente.class) final WSSortParam sort,
-            @Parameter(schema = @Schema(defaultValue = Utils.DEFAULT_REST_PAGE))
-                @QueryParam("page") @DefaultValue(Utils.DEFAULT_REST_PAGE) final Integer page,
-            @Parameter(schema = @Schema(defaultValue = Utils.DEFAULT_REST_LIMIT))
-                @QueryParam("limit") @DefaultValue(Utils.DEFAULT_REST_LIMIT) final Integer limit)
+            @Parameter() @QueryParam("id") final Long id, @Parameter() @QueryParam("nome")
+            final String nome,
+            @Parameter(
+                    schema = @Schema(
+                            implementation = String.class,
+                            enumeration = { "ATIVO", "INATIVO" })) @QueryParam("situacao") final Situacao situacao,
+            @Parameter() @QueryParam("cpf") final String cpf,
+            @Parameter(example = "2001-11-01") final Date dataNascimento,
+            @Parameter(
+                    schema = @Schema(
+                            implementation = String.class,
+                            defaultValue = "id,nome")) @QueryParam("fields") @DefaultValue("id,nome") @WSParamFormat(V1ResponsePaciente.class)
+            final WSFieldParam fields,
+            @Parameter(
+                    schema = @Schema(
+                            implementation = String.class,
+                            defaultValue = "nome:asc")) @QueryParam("sort") @DefaultValue("nome:asc") @WSParamFormat(V1ResponsePaciente.class)
+            final WSSortParam sort,
+            @Parameter(
+                    schema = @Schema(
+                            defaultValue = Utils.DEFAULT_REST_PAGE)) @QueryParam("page") @DefaultValue(Utils.DEFAULT_REST_PAGE)
+            final Integer page,
+            @Parameter(
+                    schema = @Schema(
+                            defaultValue = Utils.DEFAULT_REST_LIMIT)) @QueryParam("limit") @DefaultValue(Utils.DEFAULT_REST_LIMIT)
+            final Integer limit)
             throws ApplicationException {
 
-        return new ResponsePaciente().converter(pacienteBusiness.paginarPaciente(id, nome, situacao,
-                fields.getListField(), sort.getListSort(), page, limit));
+        return new V1ResponsePaciente().converter(pacienteBusiness.paginarPaciente(id, nome, situacao, cpf,
+                dataNascimento, fields.getListField(), sort.getListSort(), page, limit));
     }
 }
